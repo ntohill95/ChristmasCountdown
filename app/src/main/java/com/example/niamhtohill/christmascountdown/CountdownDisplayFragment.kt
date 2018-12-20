84cb749fdc382e85127e2e6b0cdbb2469f5ffb7e
ntohill95 <ntohill95@gmail.com>
2018-12-20 16:22:29 +0000

    Countdown to christmas now working with Lottie animation on fragment

--- app/src/main/java/com/example/niamhtohill/christmascountdown/CountdownDisplayFragment.kt
+++ app/src/main/java/com/example/niamhtohill/christmascountdown/CountdownDisplayFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.niamhtohill.christmascountdown.ui.maincountdown.MainCountdownViewModel
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.countdown_display_fragment.*
 
 class CountdownDisplayFragment : Fragment() {
 
@@ -13,9 +15,25 @@ class CountdownDisplayFragment : Fragment() {
         fun newInstance() = CountdownDisplayFragment()
     }
 
-    private lateinit var viewModel: MainCountdownViewModel
+    private lateinit var viewModel: CountdownViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        viewModel = ViewModelProviders.of(this).get(CountdownViewModel::class.java)
+
+        viewModel.fetchTime()
+
+        viewModel.daysLeft.observe(this, Observer {
+            days_textview.text = viewModel.daysLeft.value
+        })
+        viewModel.hoursLeft.observe(this, Observer {
+            hours_textview.text = viewModel.hoursLeft.value
+        })
+        viewModel.minutesLeft.observe(this, Observer {
+            minutes_textview.text = viewModel.minutesLeft.value
+        })
+        viewModel.secondsLeft.observe(this, Observer {
+            seconds_textview.text = viewModel.secondsLeft.value
+        })
         return inflater.inflate(R.layout.countdown_display_fragment, container, false)
     }
 }
---
+++ app/src/main/java/com/example/niamhtohill/christmascountdown/CountdownViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.niamhtohill.christmascountdown
+
+import android.os.CountDownTimer
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CountdownViewModel : ViewModel() {
+
+    var daysLeft = MutableLiveData<String>().apply { postValue("0 days") }
+    var hoursLeft = MutableLiveData<String>().apply { postValue("0 hours") }
+    var minutesLeft = MutableLiveData<String>().apply { postValue("0 minutes") }
+    var secondsLeft = MutableLiveData<String>().apply { postValue("0 seconds") }
+
+    fun fetchTime() {
+        val formatter = SimpleDateFormat("dd.MM.yyyy, HH:mm:ss")
+        formatter.isLenient = false
+
+        val endTime = "25.12.2018, 00:00:00"
+        lateinit var endDate: Date
+        var milliseconds = 0L
+
+        try {
+            endDate = formatter.parse(endTime)
+            milliseconds = endDate.time
+        } catch (e: ParseException) {
+            println(e.message)
+        }
+        var startTime = System.currentTimeMillis()
+        var diff = milliseconds - startTime
+
+        val countDownTimer = object : CountDownTimer(milliseconds, 1000) {
+            override fun onTick(p0: Long) {
+                startTime -= 1
+                val serverUpTimeSeconds = (p0 - startTime) / 1000
+                daysLeft.postValue((String.format("%d", serverUpTimeSeconds / 86400)) + " days")
+                hoursLeft.postValue((String.format("%d", (serverUpTimeSeconds % 86400) / 3600)) + " hours")
+                minutesLeft.postValue((String.format("%d", (((serverUpTimeSeconds % 86400) % 3600) / 60))) + " minutes")
+                secondsLeft.postValue((String.format("%d", (((serverUpTimeSeconds % 86400) % 3600) % 60))) + " seconds")
+            }
+
+            override fun onFinish() {
+            }
+        }
+        countDownTimer.start()
+    }
+}
